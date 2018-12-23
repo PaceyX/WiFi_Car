8203c7ab7148c0ef45021b9e53f37bd1dc62262e
Pacey.Pei <peixujkl@gmail.com>
2018-12-23 19:58:17 +0800

    电机PWM驱动调通

--- Bsp/motor_bsp.c
+++ Bsp/motor_bsp.c
@@ -6,7 +6,12 @@
 void Motor_Init(void)
 {
 	HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
-	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
+	HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2);
 	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
 	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_4);
+	
+	TIM2->CCR1 = 0;
+	TIM2->CCR2 = 200;
+	TIM3->CCR3 = 0;
+	TIM3->CCR4 = 200;
 }
