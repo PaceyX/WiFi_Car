311f7df8c24439da45297c0a7faa87f15c3cd881
Pacey <px@imscv.com>
2018-12-28 09:37:42 +0800

    电机结构体注释修改

--- Bsp/motor_bsp.c
+++ Bsp/motor_bsp.c
@@ -1,8 +1,38 @@
 #include "motor_bsp.h"
 #include "tim.h"
 
+
+typedef struct MotorsParamsType
+{
+	__IO u32 *PWM_CHA;
+	__IO u32 *PWM_CHB;
+    u8 encoder_dir;             			/* 当前编码器方向. */
+    u8 motor_dir;							/* 当前电机的转向，通过IMU的加速度来判断. */
+	u8 motor_num;							/* 当前电机数量. */
+	u8 motor_dir_real;						/* 电机真实的控制方向. */
+    s16 velocity_factor;					/* 里程计校准因子. */
+	
+//	s32 present_speed;						/* 当前车子速度. */
+    s32 acccumulated_distance;				/* 当前最新一次的里程计距离. */
+    s32 accumulated_distance_remainder;		/* 之前累计的里程计距离. */
+	
+	s32 velocity;							/* 当前车子速度. */
+	s32 last_motor_output;					/* 上一次的电机PWM开环输出. */
+
+	// Speed loop
+    s32 targetVelocity;						/* 闭环控制的目标线速度. */
+    s32 cur_pwm_out;						/* 闭环控制的当前PWM输出. */
+	s32 last_pid_pwm_output;				/* 上一次的电机PWM闭环输出. */
+    PIDObjTyp velocityPID;					/* 闭环控制的电机PID参数. */
+} MotorParamsTypedef;
+
 MotorParamsTypedef MotorParams[2];
 
+
+typedef struct MotorSpeedTypedef
+{
+	
+} MotorSpeedTypedef;
 
 
 void Motor_Init(void)
@@ -13,9 +43,9 @@ void Motor_Init(void)
 	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_4);
 	
 	TIM2->CCR1 = 0;
-	TIM2->CCR2 = 200;
+	TIM2->CCR2 = 0;
 	TIM3->CCR3 = 0;
-	TIM3->CCR4 = 200;
+	TIM3->CCR4 = 0;
 }
 
 void WheelCalSpeed_IRQHandler(uint16_t GPIO_Pin)
--- Bsp/motor_bsp.h
+++ Bsp/motor_bsp.h
@@ -6,30 +6,7 @@
 #include "pid_regulators.h"
 
 
-typedef struct MotorsParamsType
-{
-	__IO u32 *PWM_CHA;
-	__IO u32 *PWM_CHB;
-    u8 encoder_dir;             			/* 当前电机编码器的方向. */
-    u8 motor_dir;							/* 电机的目标方向. */
-	u8 motor_num;							/* 电机编号定义. */
-	u8 motor_dir_real;						/* 电机的真实方向. */
-    s16 velocity_factor;					/* 速度校准因子. */
-	
-	s32 present_speed;						/* 当前脉冲速度. */
-    s32 acccumulated_distance;				/* 新的一次里程计数据. */
-    s32 accumulated_distance_remainder;		/* 累计的里程计数据. */
-	
-	s32 last_motor_output;					/* 记录开环上一次PWM输出. */
-	s32 last_pid_pwm_output;				/* 记录闭环上一次PWM输出. */
-	
-	// Speed loop
-    s32 velocity;							/* 当前电机线速度. */
-    s32 targetVelocity;						/* 当前电机目标线速度. */
-    s32 cur_pwm_out;						/* 当前电机PWM输出. */
-	
-    PIDObjTyp velocityPID;					/* 电机的PID数据赋值. */
-} MotorParamsTypedef;
+
 
 void Motor_Init(void);
 void WheelCalSpeed_IRQHandler(uint16_t GPIO_Pin);
